0f541e15a4d573a1afb2d10114396590f2c3a128
Carlos Sigua <carlos.sigua@gmail.com>
2024-01-02 01:00:58 -0500

    UN CAMBIO MAS EN VITE.CONFIG.JS

--- projects/a_app/vite.config.js
+++ projects/a_app/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '147.182.227.114',
-    port: 8080,
+    host: '0.0.0.0',
+    port: process.env.PORT  || 5173,
   },
 })
